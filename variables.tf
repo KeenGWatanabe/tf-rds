91ed736a4ff3847a357b895ecaac6552c61807dd
KeenGWatanabe <mckeenweb@gmail.com>
2025-02-02 20:27:49 +0800

    initial commit

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
-  default = "us-east-1"
+  description = "The AWS region to deploy resources"
+  type = string
 }
 
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type = string
-  default = "10.0.0.0/16"
 }
 
 variable "subnet_count" {
@@ -41,23 +41,11 @@ variable "settings" {
 variable "public_subnet_cidr" {
   description = "Available CIDR-public subnets"
   type = list(string)
-  default = [ 
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/24",
-    "10.0.4.0/24"
-   ]
 }
 
 variable "private_subnet_cidr" {
   description = "Available CIDR-private subnets"
   type = list(string)
-  default = [ 
-    "10.0.101.0/24",
-    "10.0.102.0/24",
-    "10.0.103.0/24",
-    "10.0.104.0/24"
-   ]
 }
 
 #terraform.tfvars
